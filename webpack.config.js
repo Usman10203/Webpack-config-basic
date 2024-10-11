866714850955c2e3ece8f5beb21cdc65ca329073
Usman10203 <udeveloper1234@gmail.com>
2024-10-11 20:39:30 +0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js', // Output bundle
+        clean: true, // Clean the output directory before each build
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.svg$/,
+                loader: 'svg-inline-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'], // Resolve file extensions
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html', // HTML template to include the bundle
+        }),
+    ],
+};
